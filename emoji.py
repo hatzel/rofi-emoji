8da32a411c79047cd06eaaf6a1151302f6b38109
Hans Ole Hatzel <hhatzel@gmail.com>
2017-09-20 18:08:20 +0200

    Python prototype and full list

---
+++ emoji.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import emoji_data_python
+from sys import argv
+from subprocess import call
+
+if len(argv) == 1:
+    for k, v in emoji_data_python.emoji_short_names.items():
+        print(v.name, v.char, sep="\t")
+else:
+    name, emoji = argv[1].split("\t")
+    codepoints = [ord(c) for c in emoji]
+    for c in codepoints:
+        call(["xdotool", "key", "U" + hex(c)[2:]])
